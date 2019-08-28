ca1ee96355339da0fb1d491fd3a898d7cdd5b279
Shahreen Sultana <47378713+Shahreen-Sultana-1521173042@users.noreply.github.com>
2019-08-28 21:44:52 +0600

    Update sign.php

--- sign.php
+++ sign.php
@@ -1,3 +1,21 @@
 <?php
 include_once 'dbConnection.php';
 ob_start();
+$name = $_POST['name'];
+$name= ucwords(strtolower($name));
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$college = $_POST['college'];
+$mob = $_POST['mob'];
+$password = $_POST['password'];
+$name = stripslashes($name);
+$name = addslashes($name);
+$name = ucwords(strtolower($name));
+$gender = stripslashes($gender);
+$gender = addslashes($gender);
+$email = stripslashes($email);
+$email = addslashes($email);
+$college = stripslashes($college);
+$college = addslashes($college);
+$mob = stripslashes($mob);
+$mob = addslashes($mob);
