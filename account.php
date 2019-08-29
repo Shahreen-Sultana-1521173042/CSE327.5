a060ea87a7fe356a5bed960e018420b1a81fcbdf
nazmus31 <47378691+nazmus31@users.noreply.github.com>
2019-08-29 22:26:59 +0600

    Update account.php

--- account.php
+++ account.php
@@ -9,3 +9,4 @@
  <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
  <link rel="stylesheet" href="css/main.css">
  <link  rel="stylesheet" href="css/font.css">
+ <script src="js/jquery.js" type="text/javascript"></script>
