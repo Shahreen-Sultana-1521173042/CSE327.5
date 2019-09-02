c0f9225678e0e97dd6f9fcb53824cfaeebb9e0f6
Shahreen Sultana <47378713+Shahreen-Sultana-1521173042@users.noreply.github.com>
2019-09-02 13:27:54 +0600

    Update account.php

--- account.php
+++ account.php
@@ -191,3 +191,55 @@ echo '</table></div>';
 
 }
 ?>
+<!--quiz end-->
+<?php
+//history start
+if(@$_GET['q']== 2) 
+{
+$q=mysqli_query($con,"SELECT * FROM history WHERE email='$email' ORDER BY date DESC " )or die('Error197');
+echo  '<div class="panel title">
+<table class="table table-striped title1" >
+<tr style="color:red"><td><b>S.N.</b></td><td><b>Quiz</b></td><td><b>Question Solved</b></td><td><b>Right</b></td><td><b>Wrong<b></td><td><b>Score</b></td>';
+$c=0;
+while($row=mysqli_fetch_array($q) )
+{
+$eid=$row['eid'];
+$s=$row['score'];
+$w=$row['wrong'];
+$r=$row['cor'];
+$qa=$row['level'];
+$q23=mysqli_query($con,"SELECT title FROM quiz WHERE  eid='$eid' " )or die('Error208');
+while($row=mysqli_fetch_array($q23) )
+{
+$title=$row['title'];
+}
+$c++;
+echo '<tr><td>'.$c.'</td><td>'.$title.'</td><td>'.$qa.'</td><td>'.$r.'</td><td>'.$w.'</td><td>'.$s.'</td></tr>';
+}
+echo'</table></div>';
+}
+
+//ranking start
+if(@$_GET['q']== 3) 
+{
+$q=mysqli_query($con,"SELECT * FROM rank  ORDER BY score DESC " )or die('Error223');
+echo  '<div class="panel title"><div class="table-responsive">
+<table class="table table-striped title1" >
+<tr style="color:red"><td><b>Rank</b></td><td><b>Name</b></td><td><b>Gender</b></td><td><b>College</b></td><td><b>Score</b></td></tr>';
+$c=0;
+while($row=mysqli_fetch_array($q) )
+{
+$e=$row['email'];
+$s=$row['score'];
+$q12=mysqli_query($con,"SELECT * FROM user WHERE email='$e' " )or die('Error231');
+while($row=mysqli_fetch_array($q12) )
+{
+$name=$row['name'];
+$gender=$row['gender'];
+$college=$row['college'];
+}
+$c++;
+echo '<tr><td style="color:#99cc32"><b>'.$c.'</b></td><td>'.$name.'</td><td>'.$gender.'</td><td>'.$college.'</td><td>'.$s.'</td><td>';
+}
+echo '</table></div></div>';}
+?>
